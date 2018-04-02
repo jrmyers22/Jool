52a3b48078311eb0b16bf0bb49c1057f9714ba2e
jrmyers5 <jrmyers5@ncsu.edu>
2018-04-02 16:16:18 -0400

    Add comments, app icon

--- app/src/main/java/com/example/jool/MainActivity.java
+++ app/src/main/java/com/example/jool/MainActivity.java
@@ -9,20 +9,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 
 import pl.droidsonroids.gif.GifTextView;
 
+/**
+ * Populates the sole view with the Jool image,
+ * multicolored gif (transparent), and title text.
+ * On a shake, the gif becomes visible.
+ * When the Jool is long-pressed, it "wiggles" and
+ * plays the inhale sound effect.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /** Sensor manager used for the shake event. */
     private SensorManager mSensorManager;
 
+    /** Listens for the shake event. */
     private ShakeEventListener mSensorListener;
 
+    /** Media player for the inhale/exhale sound effect. */
     private MediaPlayer mp;
 
     @Override
@@ -34,9 +41,9 @@ public class MainActivity extends AppCompatActivity {
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorListener = new ShakeEventListener();
 
-        mp = MediaPlayer.create(getApplicationContext(), R.raw.vape_trim);
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.vape_trim_short);
 
-        toast("Shake to toggle Party Mode!");
+        toast("Shake to toggle Party Mode!", true);
 
         final GifTextView colorGif = findViewById(R.id.colorGif);
         colorGif.setAlpha(0);
@@ -54,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-//        Shake works correctly
+        // Toggle the "Party Mode" visibility on shake
         mSensorListener.setOnShakeListener(new ShakeEventListener.OnShakeListener() {
 
             public void onShake() {
@@ -66,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Activate the shake and sound when image is long pressed
         final ImageView jool = findViewById(R.id.jool);
         jool.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -79,6 +87,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Registers the shake event listener
+     * when shake is initially toggled.
+     */
     @Override
     protected void onResume() {
         super.onResume();
@@ -87,14 +99,29 @@ public class MainActivity extends AppCompatActivity {
                 SensorManager.SENSOR_DELAY_UI);
     }
 
+    /**
+     * Unregisters the shake event listener
+     * when shake is toggled.
+     */
     @Override
     protected void onPause() {
         mSensorManager.unregisterListener(mSensorListener);
         super.onPause();
     }
 
-    public void toast(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    /**
+     * Wrapper to make toast message.
+     * @param msg Message to display
+     * @param isShort To display a short vs long message
+     */
+    public void toast(String msg, boolean isShort) {
+        int length = 0;
+        if (isShort) {
+            length = Toast.LENGTH_SHORT;
+        } else {
+            length = Toast.LENGTH_LONG;
+        }
+        Toast.makeText(this, msg, length).show();
     }
 
 }
