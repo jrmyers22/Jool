5ed32d73de4831cf8dfc59567289f366cbd592a8
jrmyers5 <jrmyers5@ncsu.edu>
2018-04-02 15:41:08 -0400

    Add shake and sound effect

--- app/src/main/java/com/example/jool/MainActivity.java
+++ app/src/main/java/com/example/jool/MainActivity.java
@@ -3,15 +3,17 @@ package com.example.jool;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.concurrent.TimeUnit;
 
 import pl.droidsonroids.gif.GifTextView;
 
@@ -21,29 +23,36 @@ public class MainActivity extends AppCompatActivity {
 
     private ShakeEventListener mSensorListener;
 
+    private MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("\"Juul hand me the Joel\"");
+        setTitle("Juul hand me the Joel");
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorListener = new ShakeEventListener();
 
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.vape_trim);
+
+        toast("Shake to toggle Party Mode!");
+
         final GifTextView colorGif = findViewById(R.id.colorGif);
         colorGif.setAlpha(0);
-        final TextView text = findViewById(R.id.text);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (colorGif.getAlpha() == 1) {
-                    colorGif.setAlpha(0);
-                } else {
-                    colorGif.setAlpha(1);
-                }
-            }
-        });
+
+        // Activate party mode button. Used in development.
+//        Button button = findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (colorGif.getAlpha() == 1) {
+//                    colorGif.setAlpha(0);
+//                } else {
+//                    colorGif.setAlpha(1);
+//                }
+//            }
+//        });
 
 //        Shake works correctly
         mSensorListener.setOnShakeListener(new ShakeEventListener.OnShakeListener() {
@@ -56,9 +65,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        final ImageView jool = findViewById(R.id.jool);
+        jool.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                //toast("Long click activated");
+                Animation shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
+                jool.startAnimation(shake);
+                mp.start();
+                return true;
+            }
+        });
     }
-
-
 
     @Override
     protected void onResume() {
@@ -72,6 +91,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         mSensorManager.unregisterListener(mSensorListener);
         super.onPause();
+    }
+
+    public void toast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
 }
